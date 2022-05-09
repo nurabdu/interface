99266c29782060286a88c011978947a10d8fb335
nurabdu <abdurashitov.beknur@gmail.com>
2022-05-09 21:18:39 +0300

    new folder redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+//import *as serviceWorker from './serviceWorker'
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App state={state}
+        />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+let state = {
+    usersPage: {
+        users:
+            [
+                { name: "beknur", surname: 'abdu', age: '26' },
+                { name: "beknur", surname: 'abdu', age: '26' },
+                { name: "beknur", surname: 'abdu', age: '26' },
+                { name: "beknur", surname: 'abdu', age: '26' },
+                { name: "beknur", surname: 'abdu', age: '26' }
+            ]
+    },
+    dialogPage: {
+        dialogs:
+            [
+                { id: 1, message: 'hi' },
+                { id: 2, message: 'how are you' },
+                { id: 3, message: 'where are you from?' },
+                { id: 4, message: 'opsum lorem signatura' },
+                { id: 5, message: 'mrgacom it mobile networks' },
+                { id: 6, message: 'beeline life is good' }
+            ]
+    }
+}
+
+export default state;
